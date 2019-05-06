6851c9c525b7b7b1c4029cb39dcb131d60d5ba7c
Antonio Garcia <antonio.jgarcia100@gmail.com>
2019-05-06 14:21:19 -0400

    can now switch between notes and toggle NoteDetail

--- src/NotesDetail.js
+++ src/NotesDetail.js
@@ -14,10 +14,27 @@ export default class NotesDetail extends React.Component {
     constructor(props) {
         super(props);
         this.state = { 
-            isEditing: true,
-            draftText: props.text
+            isEditing: false,
+            draftText: props.note.text,
+            id: props.note.id
         }
     }
+    static getDerivedStateFromProps(props, state) {
+        // There is no 'this' 
+        // so we receive props and state as args
+
+        // Must return an object that describes 
+        // any modifications to state. 
+        if (props.note.id !== state.id) {
+        return { 
+                id: props.note.id,
+                draftText: props.note.text
+        };
+    } else {
+        return null;
+    }
+}
+
     render() {  // Render does not receive props
         // Declares the className and note variables 
         // and assigns them to the properties from this.props
